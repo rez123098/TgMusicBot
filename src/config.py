e8d731d29767fc7e15b226ebee10c314a6814806
rez123098 <rez120938@gmail.com>
2025-05-17 20:17:37 +0700

    Update config.py

--- src/config.py
+++ src/config.py
@@ -44,7 +44,7 @@ MIN_MEMBER_COUNT: int = get_env_int("MIN_MEMBER_COUNT", 50)
 SESSION_STRINGS: list[str] = [getenv(f"STRING{i}", None) for i in range(1, 11)]
 SESSION_STRINGS = [s for s in SESSION_STRINGS if s]
 
-OWNER_ID: int = get_env_int("OWNER_ID", 5938660179)
+OWNER_ID: int = get_env_int("OWNER_ID", 6874740506)
 LOGGER_ID: int = get_env_int("LOGGER_ID", 0)
 MONGO_URI: Optional[str] = getenv("MONGO_URI")
 API_URL: Optional[str] = getenv("API_URL")
